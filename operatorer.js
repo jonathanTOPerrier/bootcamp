31b24111c18028e7c890295bf8e50aad59fc7b2f
jonathanTOPerrier <35562503+jonathanTOPerrier@users.noreply.github.com>
2018-01-22 09:25:36 +0100

    operatorere

---
+++ operatorer.js
@@ -0,0 +1,63 @@
+/*
+//øvelse
+
+let forste = 100;
+
+let anden = 20;
+
+console.log(forste+anden);
+console.log(forste-anden);
+console.log(forste/anden);
+console.log(forste*anden);
+console.log(forste%anden);
+console.log(forste++);
+console.log(forste--);
+
+//øvelse 3
+console.log('øvelse 3!!!!!');
+
+let resultat = forste+anden;
+
+console.log(resultat);
+
+resultat = forste-anden;
+
+console.log(resultat);
+
+resultat = forste*anden;
+
+console.log(resultat);
+
+resultat = forste/anden;
+
+console.log(resultat);
+
+resultat = forste%anden;
+
+console.log(resultat);
+
+//øvelse 4
+
+console.log('øvelse 4!!!');
+
+let navn = 'bob';
+
+let by = 'fiesta';
+
+if( navn == 'bob' && by == 'fiesta'){
+    console.log('ej hvor sjovt det virker, har jeg slet ikke kunnet i en måned nu');
+}else{
+    console.log('Så havner vi i else, høhøhøhøhø!!!');
+}
+
+//øvelse 4 (5)
+
+let navn = 'troels';
+
+let by = 'fiesta';
+
+if( navn == 'bob' || by == 'fiesta'){
+    console.log('ej hvor sjovt det virker, har jeg slet ikke kunnet i en måned nu');
+}else{
+    console.log('Så havner vi i else, høhøhøhøhø!!!');
+}*/
---
+++ operatorer.md
@@ -0,0 +1,19 @@
+# operatorer
+
+* =  tildelings operatorer
+* == det samme som
+* === samme som og samme data type
+* ! ikke
+* != ikke det samme som
+* !== ikke det samme som og ikke samme datatype
+* /< mindrer end
+* /> størrer end
+* /<= mindrer end eller lig med
+* />= større end eller lig med
+* /+ ligger til
+* /- trækker fra
+* /* ganger
+* / dividere
+* % modulere
+* && begge ting skal være opfyldt
+* || en af tingene skal være opfyldt
