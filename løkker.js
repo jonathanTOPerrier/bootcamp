7eaa7852cb02dc50eb56aea0032580ff8b589493
jonathanTOPerrier <35562503+jonathanTOPerrier@users.noreply.github.com>
2018-01-22 10:29:10 +0100

    løkker

---
+++ løkker.js
@@ -0,0 +1,44 @@
+//øvelse 2
+
+// let i=0;
+
+// for(i=0; i<26; console.log(i)){
+//     i++
+// }
+
+
+
+// while(i<25){
+//     i++;
+//     console.log(i);
+// }
+
+// do{
+//     i++;
+//     console.log(i);
+// }while(i<25);
+
+//øvelser 3
+
+// let per = 16;
+
+
+// do{
+// per++;
+// console.log(per);
+// }while(per<30);
+
+//øvelse 4
+
+// let aar = 2017;
+// for(aar=2017; aar>1917; console.log(aar)){
+//     aar--;
+// }
+
+//øvelse 5
+
+let ugeDage = ['mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag', 'søndag'];
+let i=0;
+for(i=0; i<ugeDage.length; i++){
+    console.log(ugeDage[i]);
+}
---
+++ løkker.md
@@ -0,0 +1,22 @@
+# Løkker
+
+Dette er en eksempel på en for, while og en do... while løkke.
+
+```javascript
+
+for(i=0; i<***; i++){
+
+    console.log(***);
+
+}
+
+while(i<***){
+    console.log(***);
+    i++;
+}
+
+do{
+    console.log(***);
+    i++;
+}while(i<***);
+```
